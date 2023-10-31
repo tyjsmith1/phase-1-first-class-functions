62d611f9dcf0dc2a4aed928aadca51a494789e2c
tylersmith <tyjsmith1@gmail.com>
2023-10-30 20:59:44 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function fn () {};
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {};
+}
